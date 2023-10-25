69c992adb6cdd0155c582f8c5e847c0439d29d59
Heinz Ulrich <heinzulrichv@gmail.com>
2023-10-25 13:04:04 -0400

    Assigning Code & Generating Password in JVSCRPT

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,22 @@
 // Assignment Code
-var length = Number(prompt("How long out would like your password to be? Must be no less than 8, and no more than 128, characters"));
-var characters = promt("What type of characters would you like your password to utilize?: UPPER-CASE, lower-case, $p3c!al Charat3r$, or Numbers?")
+var length = Number(prompt("How long out would like your password to be? Must be no less than 8, and no more than 128, characters."));
+var criteria = promt("What type of characters would you like your password to utilize?: UPPER-CASE, lower-case, $p3c!al Charat3r$, or Numbers?");
+var password = generatePassword ();
 
-// Write password to the #password input
+// Creating the password
 function writePassword() {
-  var password = generatePassword();
+  var criteria1 = "";
+  var name1 = criteria.name2();
+  if (name1 === "UPPER-CASE") {
+    criteria1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if (name1 === "lower-case") {
+    criteria1 = "abcdefghijklmnopqrstuvwxyz";
+  } else if (name1 === "$p3c!al Charat3r$") {
+    criteria1 = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  } else if (name1 = "Numbers") {
+    criteria1 = "0123456789"
+  };
+  
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
