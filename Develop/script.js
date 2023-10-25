ce4f3d86c5418371363118b736dd16ffc19bde9c
Heinz Ulrich <heinzulrichv@gmail.com>
2023-10-25 16:40:42 -0400

    setting minimums and prompts logs

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,41 @@
+
+
 const button = document.querySelector('button');
 
 button.addEventListener('click', function() {
-    // Promts to appear when button is clicked
-const userInput = prompt("How many characters would you like your Password to be? It may be no less than 8, and no more than 128, character in length.");
-var specialCase = prompt("Do you want special characters?");
-var upperCase = prompt("Do you want upper-case letters?");
-var lowerCase = prompt("Do you want lower-case letters?");
-var numeric = prompt("Do you want numeric characters?");
+
+// Promts to appear when button is clicked
+var passwordLength = prompt("How many characters would you like your Password to be? It may be no less than 8, and no more than 128, character in length.");
+var specialCase = confirm("Do you want special characters?");
+var upperCase = confirm("Do you want upper-case letters?");
+var lowerCase = confirm("Do you want lower-case letters?");
+var numeric = confirm("Do you want numeric characters?");
+
+console.log(passwordLength);
+
 });
 
-console.log(UserInput);
 
+println(passwordLegnth);
+println(specialCase);
+println(upperCase);
+println(lowerCase);
+println(numeric);
 
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
+
+  // Minimum count for userInput(total characters), passwordlength, spcialCase, upperCase, lowerCase, & numeric
+  var minimumCount = 0;
+  
+  // Empty minimums for spcialCase, upperCase, lowerCase & numeric
+  var minimumspecialCase = "";
+  var minimumupperCase = "";
+  var minimumlowerCase = "";
+  var minimumnumeric = "";
 
--- README.md
+++ README.md
@@ -3,9 +3,14 @@
 
 
 
+
 ## Reference Links for JavaScript
 
 * [How to trigger a promtp on button click in JavaScript](https://www.shecodes.io/athena/11102-how-to-trigger-a-prompt-on-button-click-in-javascript#:~:text=First%2C%20you'll%20need%20to,you%20can%20display%20the%20prompt.&text=%2F%2F%20Get%20the%20button%20element,for%20the%20click%20event%20button.)
 
+* [Stack Overflow: Random Password Generator - Used for confirmation code](https://stackoverflow.com/questions/62627469/random-password-generator-with-prompts)
 
-https://stackoverflow.com/questions/62627469/random-password-generator-with-prompts
+
+
+
+* [println code](https://codehs.com/tutorial/rachel/user-input-in-javascript)
