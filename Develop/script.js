dc161831baa5cb7d0fd183ef82d041cb20d81e18
Heinz Ulrich <heinzulrichv@gmail.com>
2023-10-25 12:17:27 -0400

    getting prompts together in JVSCRPT

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var length = Number(prompt("How long out would like your password to be? Must be no less than 8, and no more than 128, characters"));
+var characters = promt("What type of characters would you like your password to utilize?: UPPER-CASE, lower-case, $p3c!al Charat3r$, or Numbers?")
 
 // Write password to the #password input
 function writePassword() {
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Rodnam-Proswasd-Gnereotar
+
+
+
+
+## Reference Links for JavaScript
+
+* [How to trigger a promtp on button click in JavaScript](https://www.shecodes.io/athena/11102-how-to-trigger-a-prompt-on-button-click-in-javascript#:~:text=First%2C%20you'll%20need%20to,you%20can%20display%20the%20prompt.&text=%2F%2F%20Get%20the%20button%20element,for%20the%20click%20event%20button.)
+
+
