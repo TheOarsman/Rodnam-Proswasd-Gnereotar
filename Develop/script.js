bf1edf6812a5a996809d4208cfd07d7de2e7bcce
Heinz Ulrich <heinzulrichv@gmail.com>
2023-10-25 22:20:09 -0400

    Cleaning up JVSCRPT

--- Develop/script.js
+++ Develop/script.js
@@ -10,6 +10,22 @@ button.addEventListener('click', function () {
 
     // Promts to appear when button is clicked
     var passwordLength = prompt("How many characters would you like your Password to be? It may be no less than 8, and no more than 128, character in length.");
+
+    passwordLength = parseInt(passwordLength, 10);
+    if (isNaN(passwordLength)) {
+        alert("You were supposed to enter a number. Nothing else!");
+        return;
+    }
+
+    if (passwordLength < 8) { 
+        alert("You were supposed to select 8 or more characters!");
+        return;
+    }
+    if (passwordLength > 128) { 
+        alert("You were supposed to select no more than 128 characters!");
+        return;
+    } 
+
     var specialCase = confirm("Do you want special characters?");
     var upperCase = confirm("Do you want upper-case letters?");
     var lowerCase = confirm("Do you want lower-case letters?");
@@ -25,28 +41,45 @@ button.addEventListener('click', function () {
 });
 
 function getPasswordChar(options) {
-    options[Math.floor(Math.random() * options.length)];
+    return options[Math.floor(Math.random() * options.length)];
 }
 
 
 function generatePassword(passwordLength, specialCase, upperCase, lowerCase, numeric) {
     var password = "";
-
+    
     var specialCaseAdded = false;
     var upperCaseAdded = false;
     var lowerCaseAdded = false;
     var numericAdded = false;
 
-    for (var i = 0; i < password.length; i++) {
-        if (specialCase) {
-            if (!specialCaseAdded) {
-                password += getPasswordChar(specialCaseOptions);
-                specialCaseAdded = true;
-            } else {
-                
-            }
+    for (var i = 0; i < passwordLength; i++) {
+        if (specialCase && !specialCaseAdded) {
+            
+            password += getPasswordChar(specialCaseOptions);
+            specialCaseAdded = true;
+            continue;
         }
-
+        if (upperCase && !upperCaseAdded) {
+            
+            password += getPasswordChar(upperCaseOptions);
+            upperCaseAdded = true;
+            continue;
+        }
+        if (lowerCase && !lowerCaseAdded) {
+            
+            password += getPasswordChar(lowerCaseOptions);
+            lowerCaseAdded = true;
+            continue;
+        }
+        if (numeric && !numericAdded) {
+            
+            password += getPasswordChar(numericOptions);
+            numericAdded = true;
+            continue;
+        }
+        password += getPasswordChar(specialCaseOptions + upperCaseOptions + lowerCaseOptions + numericOptions);
+        console.log(password);
     }
 
 
