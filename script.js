0a75d5238eece173d91d93439c1743f692442703
TheOarsman <heinzulrichv@gmail.com>
2024-02-12 21:22:34 -0500

    Isolated character sets to return ONLY what is selected. Added footer with icon links to portfolios

--- script.js
+++ script.js
@@ -1,87 +1,81 @@
-var specialCaseOptions = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var upperCaseOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCaseOptions = "abcdefghijklmnopqrstuvwxyz";
-var numericOptions = "0123456789";
+const specialCaseOptions = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+const upperCaseOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCaseOptions = "abcdefghijklmnopqrstuvwxyz";
+const numericOptions = "0123456789";
 
-const button = document.querySelector('button');
-const passwordArea = document.querySelector('#password');
+const button = document.querySelector("button");
+const passwordArea = document.querySelector("#password");
 
-button.addEventListener('click', function () {
+button.addEventListener("click", function () {
+  // Prompts to appear when button is clicked
+  let passwordLength = prompt(
+    "How many characters would you like your Password to be? It may be no less than 8, and no more than 128, character in length."
+  );
 
-    // Promts to appear when button is clicked
-    var passwordLength = prompt("How many characters would you like your Password to be? It may be no less than 8, and no more than 128, character in length.");
+  passwordLength = parseInt(passwordLength, 10);
+  if (isNaN(passwordLength)) {
+    alert("You were supposed to enter a number. Nothing else!");
+    return;
+  }
 
-    passwordLength = parseInt(passwordLength, 10);
-    if (isNaN(passwordLength)) {
-        alert("You were supposed to enter a number. Nothing else!");
-        return;
-    }
+  if (passwordLength < 8) {
+    alert("You were supposed to select 8 or more characters!");
+    return;
+  }
+  if (passwordLength > 128) {
+    alert("You were supposed to select no more than 128 characters!");
+    return;
+  }
 
-    if (passwordLength < 8) { 
-        alert("You were supposed to select 8 or more characters!");
-        return;
-    }
-    if (passwordLength > 128) { 
-        alert("You were supposed to select no more than 128 characters!");
-        return;
-    } 
+  const specialCase = confirm("Do you want special characters?");
+  const upperCase = confirm("Do you want upper-case letters?");
+  const lowerCase = confirm("Do you want lower-case letters?");
+  const numeric = confirm("Do you want numeric characters?");
 
-    var specialCase = confirm("Do you want special characters?");
-    var upperCase = confirm("Do you want upper-case letters?");
-    var lowerCase = confirm("Do you want lower-case letters?");
-    var numeric = confirm("Do you want numeric characters?");
-
-    console.log(passwordLength);
-    console.log(specialCase);
-    console.log(upperCase);
-    console.log(lowerCase);
-    console.log(numeric);
-    const password = generatePassword(passwordLength, specialCase, upperCase, lowerCase, numeric);
-    passwordArea.textContent = password;
+  const password = generatePassword(
+    passwordLength,
+    specialCase,
+    upperCase,
+    lowerCase,
+    numeric
+  );
+  passwordArea.textContent = password;
 });
 
 function getPasswordChar(options) {
-    return options[Math.floor(Math.random() * options.length)];
+  return options[Math.floor(Math.random() * options.length)];
 }
 
+function generatePassword(
+  passwordLength,
+  specialCase,
+  upperCase,
+  lowerCase,
+  numeric
+) {
+  let charset = "";
+  let password = "";
 
-function generatePassword(passwordLength, specialCase, upperCase, lowerCase, numeric) {
-    var password = "";
-    
-    var specialCaseAdded = false;
-    var upperCaseAdded = false;
-    var lowerCaseAdded = false;
-    var numericAdded = false;
+  if (specialCase) {
+    charset += specialCaseOptions;
+  }
+  if (upperCase) {
+    charset += upperCaseOptions;
+  }
+  if (lowerCase) {
+    charset += lowerCaseOptions;
+  }
+  if (numeric) {
+    charset += numericOptions;
+  }
 
-    for (var i = 0; i < passwordLength; i++) {
-        if (specialCase && !specialCaseAdded) {
-            
-            password += getPasswordChar(specialCaseOptions);
-            specialCaseAdded = true;
-            continue;
-        }
-        if (upperCase && !upperCaseAdded) {
-            
-            password += getPasswordChar(upperCaseOptions);
-            upperCaseAdded = true;
-            continue;
-        }
-        if (lowerCase && !lowerCaseAdded) {
-            
-            password += getPasswordChar(lowerCaseOptions);
-            lowerCaseAdded = true;
-            continue;
-        }
-        if (numeric && !numericAdded) {
-            
-            password += getPasswordChar(numericOptions);
-            numericAdded = true;
-            continue;
-        }
-        password += getPasswordChar(specialCaseOptions + upperCaseOptions + lowerCaseOptions + numericOptions);
-        console.log(password);
-    }
+  if (charset === "") {
+    throw new Error("Please try again and select at least one character set.");
+  }
 
+  for (let i = 0; i < passwordLength; i++) {
+    password += getPasswordChar(charset);
+  }
 
-    return password;
+  return password;
 }
